1f70f0d9cd2d91bd1897e4d3193f260c052e90f0
Srik G <sskanthgorthy@gmail.com>
2017-12-04 14:55:22 +0530

    Update nvaas Puller.R

--- nvaas Puller.R
+++ nvaas Puller.R
@@ -7,6 +7,7 @@ lang_runner <- unlist(strsplit(lang_runner, split = ", "))
 alpha_runner <- c("catalogue-A, catalogue-B, catalogue-C, catalogue-D, catalogue-E, catalogue-F, catalogue-G, catalogue-H, catalogue-I, catalogue-J, catalogue-K, catalogue-L, catalogue-M, catalogue-N, catalogue-O, catalogue-P, catalogue-Q, catalogue-R, catalogue-S, catalogue-T, catalogue-U, catalogue-V, catalogue-W, catalogue-X, catalogue-Y, catalogue-Z, catalogue-0-9")
 alpha_runner <- unlist(strsplit(alpha_runner, split = ", "))
 
+# remember to add what site is. and add it till ".-browse/"
 vec <- c()
 for(i in 1:length(lang_runner)){
   for(j in 1:length(alpha_runner)){
@@ -17,11 +18,63 @@ for(i in 1:length(lang_runner)){
 url_catalouge <- vec
 rm(vec)
 
-artists_data <- c()
-hh_artists <- read_html(url_artists)
-catalog_artists <- html_nodes(hh_artists, xpath = "//body//div//div//section//div//div//a")
+hh_albums <- list()
+for(i in 1:length(url_catalouge)) hh_albums[[i]] <- read_html(url_catalouge[i])
+# remove the non existants manually
 
+catalog_artists <- list()
+for(i in 1:length(url_catalouge)){
+  catalog_artists[[i]] <- as.character(
+    html_nodes(
+    hh_albums[[i]], 
+    xpath = "//body//div//div//section//div//ul//li//a"))
+  print(i)}
 
+curator <- function(cell_pair){
+  alb_name <- as.character(unlist(strsplit(
+    unlist(strsplit(
+      cell_pair[1],
+      split = ">"))[2],
+    split = "<"))[1])
+  
+  alb_lang <- as.character(unlist(strsplit(cell_pair[1], split = "/"))[6])
+  
+  alb_year <- as.character(unlist(strsplit(
+    unlist(strsplit(
+      cell_pair[2],
+      split = ">"))[2],
+    split = "<"))[1])
+  
+  alb_url <- as.character(unlist(strsplit(cell_pair[1], split = "\""))[4])
+  
+ return(c(alb_name, alb_lang, alb_year, alb_url))
+}
 
+pre_curator <- function(list, num){
+  vec <- list[[num]]
+  len <- length(vec)
+  startie <- (1:(len/2))*2 - 1
+  stopie <- (1:(len/2))*2
+  
+  worthy <- curator(list[[num]][startie[1]:stopie[1]])
+  
+  for(i in 2:length(startie)){
+    worthy <- rbind(
+      worthy, 
+      curator(list[[num]][startie[i]:stopie[i]]))}
+  
+  worthy <- as.data.frame(worthy)
+  rownames(worthy) <- as.character(1:nrow(worthy))
+  colnames(worthy) <- c("alb_Name", "alb_Lang", "alb_Year", "alb_Link")
+  
+  return(worthy)
+}
 
+df_curated <- pre_curator(catalog_artists, 1)
+for(i in 1:395){
+  df_curated <- rbind(df_curated, pre_curator(catalog_artists, i))
+  print(i)
+  }
+
+df_curated <- unique(df_curated)
 
