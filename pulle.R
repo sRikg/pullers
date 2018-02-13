73d38f5c74e85336832ce7f8e33f9bb7acbf9b1d
Srik G <sskanthgorthy@gmail.com>
2018-02-13 22:25:09 +0530

    Update pulle.R

--- pulle.R
+++ pulle.R
@@ -3,8 +3,8 @@ require(rvest)
 df_list <- list()
 temp <- unique(tracks$Album_Identifier)
 for(i in 1:length(temp)) {
-df_list[[i]] <- read_html(temp[i])
-print(i)
+    df_list[[i]] <- read_html(temp[i])
+    print(i)
 }
 
 data_list <- lapply(
@@ -14,14 +14,15 @@ data_list <- lapply(
             df_list[[i]],
             xpath = "//body//section//div//div//div//ul//li//div//span//a"
         ))
-)
+    )
 
 c(254, 297, 298, 299, 300, 301, 429)
 
 for(i in 430:length(data_list)){
     data_list[[i]] <- data_list[[i]][1:(length(data_list[[i]]) - 2)]
 }
-636, 474
+
+
 track_list <- list()
 for(j in 1:length(data_list)){
     track_list[[j]] <- sapply(
@@ -30,7 +31,7 @@ for(j in 1:length(data_list)){
             data_list[[j]][i],
             split = "       "
         )))[4]
-    )
+        )
     print(j)
 }
 
@@ -52,18 +53,20 @@ for(i in c(1:253, 255:296, 302:428, 430:length(track_list))){
     tracklist[[i]] <- sapply(
         1:length(track_list[[i]]), 
         function(k) unlist(strsplit(
-            track_list[[i]][k], 
+            track_list[[i]][k],
             split = " \\( "
         ))
-    )
+        )
 }
 
 #474
 for(i in c(1:253, 255:296, 302:428, 430:length(track_list))){
     tracklist[[i]][2,] <- sapply(
         1:ncol(tracklist[[i]]),
-        function(k) unlist(strsplit(tracklist[[i]][2,k], split = " \\)"))
-    )
+        function(k) unlist(strsplit(
+            tracklist[[i]][2,k],
+            split = " \\)"
+        )))
 }
 
 for(i in c(1:253, 255:296, 302:428, 430:length(track_list))) {
@@ -77,15 +80,23 @@ for(j in c(1:253, 255:296, 302:428, 430:length(track_list))){
             tracklist[[j]][i,1],
             split = "[0-9]\\. "
         ))[2]
-     )
+        )
 }
 
 for(j in c(1:253, 255:296, 302:428, 430:length(track_list))){
     tracklist[[j]][,2] <- sapply(
         1:nrow(tracklist[[j]]),
-        function(i) sum(as.integer(unlist(strsplit(
-            tracklist[[j]][i,2],
-            split= ":"))) * c(60,1))
-        )
-    }
+        function(i) sum(
+            as.integer(unlist(strsplit(
+                tracklist[[j]][i,2],
+                split= ":"))) * c(60,1)))
+}
+
+
+
+
+
+
+
+
 
